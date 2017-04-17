de7778cb161618beece6e38cc3c8b2fee72d8c28
Xuanran Zong <zxr88826@gmail.com>
2017-04-16 21:35:46 -0700

    more check in

--- shell.c
+++ shell.c
@@ -1,6 +1,9 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 
 // constants
@@ -8,17 +11,19 @@
 #define CMD_MAX_COUNT   32
 #define PATH_MAX        1024
 #define MAX_ARG         64
+#define MAX_NUM_CMDS    2
 
 // structs
 struct Cmd {
     char cmd[CMD_MAX_LEN];
+    char *infile;
+    char *outfile;
     int infd;
     int outfd;
 };
 
 // global vars
 char cdir[PATH_MAX];
-struct Cmd cmds[256];
 
 void printPrompt() {
     printf("%s $ ", cdir);
@@ -31,13 +36,14 @@ void readCmd(char *cmd) {
 }
 
 int parseCmd(char *rawCmd, struct Cmd *cmds) {
+    memset(cmds, 0, sizeof(struct Cmd) * MAX_NUM_CMDS);
     return 0;
 }
 
-void evalCmd(char *cmd, int infd, int outfd) {
-    if (!strncmp(cmd, "cd ", 3)) {
+void runCmd(struct Cmd *c) {
+    if (!strncmp(c->cmd, "cd ", 3)) {
         // builtin function for changing directory
-        char *newDir = cmd + 3;
+        char *newDir = c->cmd + 3;
         if (!newDir) {
             return;
         } else if (newDir[0] != '/') {
@@ -50,7 +56,7 @@ void evalCmd(char *cmd, int infd, int outfd) {
             chdir(newDir);
         }
         getcwd(cdir, PATH_MAX);
-    } else if (!strcmp(cmd, "about")) {
+    } else if (!strcmp(c->cmd, "about")) {
         // about
         printf("Jiaoni Zhou\nW1189742\n");
     } else {
@@ -58,31 +64,107 @@ void evalCmd(char *cmd, int infd, int outfd) {
         int pid;
         if ((pid = fork())) {
             int status;
-            waitpid(pid, &status, 0);
-        } else {
+            if (waitpid(pid, &status, 0) == -1) {
+                perror(strerror(status));
+                exit(-1);
+            }
+        } else if (pid > 0) {
+            // setup the correct fd to implement pipe and redirection
+            if (c->infd != -1) {
+                if (dup2(c->infd, 0) == -1) {
+                    perror(strerror(errno));
+                    exit(-1);
+                } else {
+                    close(c->infd);
+                }
+            }
+            if (c->outfd != -1) {
+                if (dup2(c->outfd, 1) == -1) {
+                    perror(strerror(errno));
+                    exit(-1);
+                } else {
+                    close(c->outfd);
+                }
+            }
+
             char *argv[MAX_ARG];
             int i = 0;
-            char *token = strtok(cmd, " ");
+            char *token = strtok(c->cmd, " ");
             while(token) {
                 argv[i++] = token;
                 token = strtok(NULL, " " );
             }
             argv[i] = NULL;
-            execvp(argv[0], argv);
+            if (execvp(argv[0], argv) == -1) {
+                perror(strerror(errno));
+                exit(-1);
+            };
+        } else {
+            perror(strerror(errno));
+            exit(-1);
         }
     }
 }
 
+void evalCmds(struct Cmd *cmds, int numCmd) {
+    for(int i = 0; i < numCmd; ++i) {
+        struct Cmd *cmd = &cmds[i];
+        if (cmd->infile) {
+            cmd->infd = open(cmd->infile, O_RDONLY);
+        } else {
+            cmd->infd = -1;
+        }
+        if (cmd->outfile) {
+            cmd->outfd = open(cmd->outfile, O_WRONLY);
+        } else {
+            cmd->outfd = -1;
+        }
+    }
+    if (numCmd == 2) {
+        int pipefd[2];
+        if (pipe(pipefd) == -1) {
+            perror(strerror(errno));
+            exit(-1);
+        }
+        cmds[0].outfd = pipefd[0];
+        cmds[1].infd = pipefd[1];
+    }
+}
+
+
 void shell() {
     getcwd(cdir, PATH_MAX);
     char rawCmd[CMD_MAX_LEN];
+    struct Cmd cmds[MAX_NUM_CMDS];
     while(1) {
         printPrompt();
         readCmd(rawCmd);
-        //int inputFd = getInputFromCmd(rawCmd);
-        //int outputFd = getOutputFromCmd(rawCmd);
-        //char *realCmd = 
-        evalCmd(rawCmd, 1, 0);
+        int numCmd = parseCmd(rawCmd, cmds);
+        if (numCmd > 2) {
+            fprintf(stderr, "Sorry, only support two commands connecting through pipe");
+        }
+
+        // empty command
+        if (numCmd == 0) {
+            continue;
+        }
+
+        // Figure out the input fd and output fd for each command. Set up pipe if necessary
+        evalCmds(cmds, numCmd);
+
+        for(int i = 0; i < numCmd; ++i) {
+            runCmd(&cmds[i]);
+        }
+        // Close the fd if there is any
+        for(int i = 0; i < numCmd; ++i) {
+            if (cmds[i].infd != -1) {
+                close(cmds[i].infd);
+            }
+            if (cmds[i].outfd != -1) {
+                close(cmds[i].outfd);
+            }
+        }
+
     }
 }
 
